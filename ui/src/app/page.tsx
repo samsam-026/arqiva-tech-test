1985ed8ffdba6bccc74a3cf993d0fc7413ff7ef9
Sameeha Rahman <rahman.sameeha@gmail.com>
2025-05-20 19:33:41 +0100

    Add form filtering and pagination

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import RootLayout from "./layout";
-import { Button, Card, Col, Container, Form, Row, Spinner } from "react-bootstrap";
+import { Button, ButtonGroup, Card, Col, Container, Form, Row, Spinner } from "react-bootstrap";
 import { useEffect, useRef, useState } from "react";
 import { Contribution, ContributionResponse } from "@/models/contributions";
 import ContributionItem from "@/components/ContributionsItem";
@@ -15,34 +15,63 @@ export default function Home() {
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
   const [hasMore, setHasMore] = useState(true);
+  const [numPages, setNumPages] = useState(1);
   const [orderBy, setOrderBy] = useState(ContributionOrder.Id);
-  const containerRef = useRef<HTMLDivElement>(null);
+  const [title, setTitle] = useState('');
+  const [owner, setOwner] = useState('');
+  const [description, setDescription] = useState('');
+  const scrollTopRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     setLoading(true);
     const skip = (page - 1) * PAGE_SIZE;
-    fetch(`${ENDPOINT}?skip=${skip}&limit=${PAGE_SIZE}&order_by=${orderBy}`)
+    let endpoint = `${ENDPOINT}?skip=${skip}&limit=${PAGE_SIZE}&order_by=${orderBy}`;
+    if (title.length > 0) {
+      endpoint += `&title=${title}`;
+    }
+    if (owner.length > 0) {
+      endpoint += `&owner=${owner}`;
+    }
+    if (description.length > 0) {
+      endpoint += `&description=${description}`;
+    }
+    if (title.length > 0 || owner.length > 0 || description.length > 0) {
+      endpoint += `&match=all`;
+    }
+    fetch(endpoint)
       .then((response) => response.json())
       .then((data: ContributionResponse) => {
         setContributions(data.contributions);
         setHasMore(data.total > (page * PAGE_SIZE));
+        setNumPages(Math.ceil(data.total / PAGE_SIZE));
       })
       .catch((error) => {
         console.error("Error fetching contributions:", error);
       })
       .finally(() => {
-        containerRef.current?.scrollIntoView({ behavior: "smooth" });
+        scrollTopRef.current?.scrollIntoView({ behavior: "smooth" });
         setLoading(false);
       });
-  }, [page, orderBy]);
+  }, [page, orderBy, title, owner, description]);
 
   const getNextPage = () => setPage((prev) => prev + 1);
   const getPrevPage = () => setPage((prev) => Math.max(prev - 1, 1));
+
   const handleOrderByChange = (selectedOrder: ContributionOrder) => {
-    console.log("Selected order:", selectedOrder);
-    
     setOrderBy(selectedOrder);
     setPage(1); // Reset to first page when changing order
+  };
+  const handleTitleChange = (title: string) => {
+    setTitle(title);
+    setPage(1); // Reset to first page when changing title
+  };
+  const handleOwnerChange = (owner: string) => {
+    setOwner(owner);
+    setPage(1); // Reset to first page when changing owner
+  };
+  const handleDescriptionChange = (description: string) => {
+    setDescription(description);
+    setPage(1); // Reset to first page when changing description
   };
 
   return (
@@ -51,21 +80,40 @@ export default function Home() {
         <Container className="h-100">
           <Row >
             <Col>
-              <h1 className="text-center">Contributions</h1>
+              <h1 className="text-center" ref={scrollTopRef}>Contributions</h1>
               <Card>
                 <Card.Body>
                   <Form>
                     <Row>
-                      <Col md={4}></Col>
-                      <Col lg={6} md={4}></Col>
-                      <Col lg={2} md={4}>
-                        <Form.Select aria-label="Order by" value={orderBy} onChange={(e) => handleOrderByChange(e.target.value as ContributionOrder)}>
-                          {Array.from(ContributionOrderOptions.entries()).map(([key, value]) => (
-                            <option key={key} value={key}>
-                              {value}
-                            </option>
-                          ))}
-                        </Form.Select>
+                      <Col lg={3} md={4}>
+                        <Form.Group className="mb-3 my-md-3">
+                          <Form.Label>Title</Form.Label>
+                          <Form.Control value={title} onChange={(e) => handleTitleChange(e.target.value)}></Form.Control>
+                        </Form.Group>
+                      </Col>
+                      <Col lg={3} md={4}>
+                        <Form.Group className="my-3">
+                          <Form.Label>Owner</Form.Label>
+                          <Form.Control value={owner} onChange={(e) => handleOwnerChange(e.target.value)}></Form.Control>
+                        </Form.Group>
+                      </Col>
+                      <Col lg={3} md={4}>
+                        <Form.Group className="my-3">
+                          <Form.Label>Description</Form.Label>
+                          <Form.Control value={description} onChange={(e) => handleDescriptionChange(e.target.value)}></Form.Control>
+                        </Form.Group>
+                      </Col>
+                      <Col lg={3} md={4}>
+                        <Form.Group className="mt-3 my-md-3">
+                          <Form.Label>Order by</Form.Label>
+                          <Form.Select value={orderBy} onChange={(e) => handleOrderByChange(e.target.value as ContributionOrder)}>
+                            {Array.from(ContributionOrderOptions.entries()).map(([key, value]) => (
+                              <option key={key} value={key}>
+                                {value}
+                              </option>
+                            ))}
+                          </Form.Select>
+                        </Form.Group>
                       </Col>
                     </Row>
                   </Form>
@@ -75,21 +123,31 @@ export default function Home() {
           </Row>
         </Container>
       </header>
-      <Container className="container-contributions" ref={containerRef}>
-
-        {loading ? <Spinner animation="border" /> : <>
+      <Container className="container-contributions" >
+        {loading ? <div className="py-5 text-center"><Spinner animation="border" /></div> : <>
           {contributions.length > 0 ? <Row>
             {contributions.map((item: any, index: number) => (
               <Col lg={4} md={6} className="contribution-item" key={index}>
                 <ContributionItem item={item} />
               </Col>
             ))}
-          </Row> : <p className="m-auto">No contributions</p>}
+          </Row> : <p className="text-center my-5">No contributions</p>}
+
           <Row className="my-3">
             <Col className="text-center">
-              <Button disabled={page === 1} className="me-3" onClick={getPrevPage}>&#10094;</Button>
-              {page}
-              <Button disabled={!hasMore} className="ms-3" onClick={getNextPage}>&#10095;</Button>
+              <ButtonGroup>
+                <Button disabled={page === 1} onClick={getPrevPage}>&#10094;</Button>
+                {Array.from({ length: numPages }, (_, i) => (
+                  <Button
+                    key={i}
+                    onClick={() => setPage(i + 1)}
+                    variant={page === (i + 1) ? "success" : "primary"}
+                  >
+                    {i + 1}
+                  </Button>
+                ))}
+                <Button disabled={!hasMore} onClick={getNextPage}>&#10095;</Button>
+              </ButtonGroup>
             </Col>
           </Row>
         </>}
