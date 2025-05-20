f9daf9ba5315fd8e99452c1a3b2ba945f7d73e6c
Sameeha Rahman <rahman.sameeha@gmail.com>
2025-05-20 01:00:50 +0100

    Alter layout and home page

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,95 +1,90 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import RootLayout from "./layout";
+import { Button, ButtonGroup, Col, Container, Row, Spinner } from "react-bootstrap";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import { Component } from "react";
+import { Contribution, ContributionResponse } from "@/models/contributions";
+import ContributionItem from "@/components/ContributionsItem";
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+const ENDPOINT = "http://localhost:8000/contributions";
+const PAGE_SIZE = 14;
+
+interface HomeState {
+  contributions: Contribution[];
+  page: number;
+  loading: boolean;
+  hasMore: boolean;
+}
+
+export default class Home extends Component<{}, HomeState> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      contributions: [],
+      page: 1,
+      loading: false,
+      hasMore: true,
+    };
+  }
+
+  componentDidMount() {
+    this.getContributions();
+  }
+
+  getNextPage() {
+    this.setState((prevState) => ({
+      page: prevState.page + 1,
+    }), () => {
+      this.getContributions();
+    });
+  }
+
+  getPrevPage() {
+    this.setState((prevState) => ({
+      page: Math.max(prevState.page - 1, 1),
+    }), () => {
+      this.getContributions();
+    });
+  }
+
+  getContributions() {
+    this.setState({ loading: true });
+    const skip = (this.state.page - 1) * PAGE_SIZE;
+    fetch(`${ENDPOINT}?skip=${skip}&limit=${PAGE_SIZE}`)
+      .then((response) => response.json())
+      .then((data: ContributionResponse) => {
+        const hasMore = data.total > (this.state.page * PAGE_SIZE);
+        this.setState({ contributions: data.contributions, hasMore });
+      })
+      .catch((error) => {
+        console.error("Error fetching contributions:", error);
+      }).finally(() => {
+        this.setState({ loading: false });
+      });
+  }
+
+  render() {
+    return (
+      <RootLayout>
+        <Container>
+          <h1>Contributions</h1>
+          {this.state.loading ? <Spinner animation="border" /> : <>
+            {this.state.contributions.length > 0 ? <Row>
+              {this.state.contributions.map((item: any, index: number) => (
+                <Col lg={4} md={6} >
+                  <ContributionItem key={index} item={item} />
+                </Col>))}
+            </Row> : <p>No contributions</p>}
+            <Row>
+              <Col className="text-center">
+                <Button disabled={this.state.page === 1} onClick={this.getPrevPage}>&lt; Previous</Button>
+                {this.state.page}
+                <Button disabled={!this.state.hasMore} onClick={this.getNextPage}>Next &gt;</Button>
+              </Col>
+            </Row>
+          </>}
+        </Container>
+      </RootLayout>
+    );
+  }
 }
