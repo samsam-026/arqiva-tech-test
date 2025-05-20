2b27ce2f5dd421526495d774d6814a9529dd5b62
Sameeha Rahman <rahman.sameeha@gmail.com>
2025-05-20 15:06:42 +0100

    Add CORS handling to backend

--- server/main.py
+++ server/main.py
@@ -5,6 +5,7 @@ from enum import Enum
 from typing import Optional, List
 
 from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
 
 # SIMPLE store of data for this exercise
 DATA = {}
@@ -19,6 +20,16 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
