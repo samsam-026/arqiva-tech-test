513eaa1c30cc351d94069c23f7c04fc5368ec868
Sameeha Rahman <rahman.sameeha@gmail.com>
2025-05-20 01:00:34 +0100

    Add models

---
+++ ui/src/models/contributions.ts
@@ -0,0 +1,15 @@
+export interface Contribution {
+    id: number,
+    title: string,
+    description: string,
+    startTime: string,
+    endTime: string,
+    owner: string
+}
+
+export interface ContributionResponse {
+    contributions: Contribution[],
+    total: number,
+    skip: number,
+    limit: number
+}
